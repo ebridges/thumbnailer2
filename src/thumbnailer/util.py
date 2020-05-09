96041e0ac8f4318879687fbf791537a85cc98f29
Edward Bridges <github@eqbridges.com>
2020-05-09 18:13:50 -0400

    refactor logging utils into separate module

---
+++ src/thumbnailer/util.py
@@ -0,0 +1,23 @@
+from logging import getLogger, basicConfig, DEBUG, INFO, debug, info
+
+
+DEFAULT_WIDTH=222
+DEFAULT_HEIGHT=222
+
+
+
+def configure_logging(verbose):
+  if verbose:
+    level = DEBUG
+  else:
+    level = INFO
+
+  if getLogger().hasHandlers():
+      # The Lambda environment pre-configures a handler logging to stderr. 
+      # If a handler is already configured, `.basicConfig` does not execute.
+      # Thus we set the level directly.
+      getLogger().setLevel(level)
+  else:
+      basicConfig(
+        format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=level
+      )
